a62703125961ecc583a495dbe57d7909cf45becd
Kouta Nakano <knakano95@gmail.com>
2022-05-10 18:59:15 +0900

    test for useEnterSubmitHook

---
+++ src/hooks/useEnterSubmit/index.test.ts
@@ -0,0 +1,24 @@
+import { cleanup, fireEvent, render, screen, renderHook } from '@testing-library/react';
+import useEnterSubmit from '.';
+
+const submit = jest.fn();
+
+describe('useEnterSubmit', () => {
+  it('should add a keydown event', () => {
+    renderHook(() => useEnterSubmit(submit, true, false));
+    fireEvent.keyDown(window, { key: 'Enter' });
+    expect(submit).toBeCalled();
+  });
+
+  it('should not add a keydown event if the condition is not met', () => {
+    renderHook(() => useEnterSubmit(submit, false, false));
+    fireEvent.keyDown(window, { key: 'Enter' });
+    expect(submit).not.toBeCalled();
+  });
+
+  it('should not add a keydown event if the response is loading', () => {
+    renderHook(() => useEnterSubmit(submit, true, true));
+    fireEvent.keyDown(window, { key: 'Enter' });
+    expect(submit).not.toBeCalled();
+  });
+});
